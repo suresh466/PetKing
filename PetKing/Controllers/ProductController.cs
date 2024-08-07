327992bdac10889aa40228de68edd4e4432841bc
Suresh <suresh.thagunna77@gmail.com>
2024-08-07 16:38:41 -0400

    pagination in products listing and improve overall design

--- PetKing/Controllers/ProductController.cs
+++ PetKing/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PetKing.Models;
+using System;
 using System.Linq;
 
 namespace PetKing.Controllers
@@ -8,13 +9,14 @@ namespace PetKing.Controllers
     public class ProductController : Controller
     {
         private readonly PetKingContext _context;
+        private readonly int PageSize = 6; // Number of products per page
 
         public ProductController(PetKingContext context)
         {
             _context = context;
         }
 
-        public IActionResult Index(int? categoryId)
+        public IActionResult Index(int? categoryId, int page = 1)
         {
             var products = _context.Products.Include(p => p.Category).AsQueryable();
 
@@ -23,12 +25,20 @@ namespace PetKing.Controllers
                 products = products.Where(p => p.CategoryID == categoryId.Value);
             }
 
+            var totalProducts = products.Count();
+            var totalPages = (int)Math.Ceiling(totalProducts / (double)PageSize);
+
+            products = products.Skip((page - 1) * PageSize).Take(PageSize);
+
             ViewBag.Categories = _context.Categories.ToList();
             ViewBag.SelectedCategoryId = categoryId;
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages = totalPages;
+
             return View(products.ToList());
         }
 
-        public IActionResult Details(int id)
+    public IActionResult Details(int id)
         {
             var product = _context.Products
                 .Include(p => p.Category)
